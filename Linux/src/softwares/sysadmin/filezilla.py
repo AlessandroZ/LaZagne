5d5d8d1f27b1285c33903e4aa3a5ce4f30a3d6b0
AlessandroZ <zanni.alessandro@gmail.com>
2015-03-19 15:18:08 +0100

    add sysadmin files
    Former-commit-id: c20470aabc99ed2d8f51deb36d68c31e70e90737 [formerly c20470aabc99ed2d8f51deb36d68c31e70e90737 [formerly c20470aabc99ed2d8f51deb36d68c31e70e90737 [formerly 1f91b4d038019ea796e3d5078cf2747985baa2d8]]]
    Former-commit-id: 3b2848341284e4179512933a763729bc7b191ab1
    Former-commit-id: aae5cee3b61c58c73313dbc065d015146e9abeaa
    Former-commit-id: 6cccb25c9fefbc89a4c837e7994a212e99bb5c43

---
+++ Linux/src/softwares/sysadmin/filezilla.py
@@ -0,0 +1,71 @@
+import xml.etree.cElementTree as ET
+from config.header import Header
+from config.write_output import print_debug, print_output
+import os
+
+class Filezilla():
+
+	def retrieve_password(self):
+		# print the title
+		Header().title_debug('Filezilla')
+		
+		directory = '~/.filezilla'
+		directory = os.path.expanduser(directory)
+		
+		interesting_xml_file = []
+		info_xml_file = []
+		if os.path.exists(os.path.join(directory, 'sitemanager.xml')):
+			interesting_xml_file.append('sitemanager.xml')
+			info_xml_file.append('Stores all saved sites server info including password in plaintext')
+		
+		if os.path.exists(os.path.join(directory, 'recentservers.xml')):
+			interesting_xml_file.append('recentservers.xml')
+			info_xml_file.append('Stores all recent server info including password in plaintext')
+		
+		if os.path.exists(os.path.join(directory, 'filezilla.xml')):
+			interesting_xml_file.append('filezilla.xml')
+			info_xml_file.append('Stores most recent server info including password in plaintext')
+		
+		if interesting_xml_file != []:
+			print_debug('INFO', 'No login and password means anonymous connection')
+			
+			pwdFound = []
+			for i in range(len(interesting_xml_file)):
+				print_debug('INFO', interesting_xml_file[i])
+				print_debug('INFO', info_xml_file[i] + '\n')
+				
+				xml_file = os.path.expanduser(directory + os.sep + interesting_xml_file[i])
+				
+				tree = ET.ElementTree(file=xml_file)
+				root = tree.getroot()
+				
+				servers = root.getchildren()
+				for ss in servers:
+					server = ss.getchildren()
+					
+					jump_line = 0
+					for s in server:
+						s1 = s.getchildren()
+						values = {}
+						for s11 in s1:
+							if s11.tag == 'Host':
+								values['Host'] = s11.text
+							
+							if s11.tag == 'Port':
+								values['Port'] = s11.text
+							
+							if s11.tag == 'User':
+								values['Login'] = s11.text
+							
+							if s11.tag == 'Pass':
+								values['Password'] = s11.text
+						
+						# write credentials into a text file
+						if len(values) != 0:
+							pwdFound.append(values)
+			# print the results
+			print_output('Filezilla', pwdFound)
+		else:
+			print_debug('INFO', 'Filezilla not installed.')
+		
+		
