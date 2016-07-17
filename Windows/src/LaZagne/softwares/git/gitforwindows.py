a8450a87b47bae7ec0ff808ca7c2f77a8794f24f
Dominique RIGHETTO <dominique.righetto@gmail.com>
2016-07-17 10:24:23 +0200

    Add account extractor for 'Git For Windows' application
    Former-commit-id: 5cd4a6d1e8b127a9c148b3fa6b6622bc09dcad36 [formerly 5cd4a6d1e8b127a9c148b3fa6b6622bc09dcad36 [formerly 5cd4a6d1e8b127a9c148b3fa6b6622bc09dcad36 [formerly ca4da35fa10d50f774e40e77b8b30cdbdc74376a]]]
    Former-commit-id: 7aecc189f638a17f24b5e414e881ae5c2e20fdd2
    Former-commit-id: 1c9905c415106686868f8a3703fb92425b54808f
    Former-commit-id: d21ff2f5d72f6a7837145d0526ee898c87bc1211

---
+++ Windows/src/LaZagne/softwares/git/gitforwindows.py
@@ -0,0 +1,69 @@
+import os
+from config.write_output import print_output, print_debug
+from config.constant import *
+from config.header import Header
+from config.moduleInfo import ModuleInfo
+from urlparse import urlparse
+
+class GitForWindows(ModuleInfo):
+
+    def __init__(self):
+        options = {'command': '-t', 'action': 'store_true', 'dest': 'gitforwindows', 'help': 'Git for Windows'}
+        ModuleInfo.__init__(self, 'gitforwindows', 'git', options)
+
+    def extract_credentials(self, location):
+        """
+        Extract the credentials from a Git store file.
+        See "https://git-scm.com/docs/git-credential-store" for file format.
+        :param location: Full path to the Git store file
+        :return: List of credentials founds
+        """
+        pwd_found = []
+        values = {}
+        if os.path.isfile(location):
+            with open(location) as f:
+                creds = f.readlines()
+                # One line have the following format: https://user:pass@example.com
+                for cred in creds:
+                    if len(cred) > 0:
+                        parts = urlparse(cred)
+                        values["Username"] = parts.username
+                        values["Password"] = parts.password
+                        values["URL"] = parts.geturl().replace(parts.username + ":" + parts.password + "@", "").strip()
+                        pwd_found.append(values)
+
+        return pwd_found
+
+    def run(self):
+        """
+        Main function
+        """
+        # Print title
+        title = "GitForWindows"
+        Header().title_info(title)
+
+        # According to the "git-credential-store" documentation:
+        # Build a list of locations in which git credentials can be stored
+        locations = []
+        locations.append(os.environ.get("USERPROFILE") + "\\.git-credentials")
+        locations.append(os.environ.get("USERPROFILE") + "\\.config\\git\\credentials")
+        if "XDG_CONFIG_HOME" in os.environ:
+            locations.append(os.environ.get("XDG_CONFIG_HOME") + "\\git\\credentials")
+
+        # Apply the password extraction on the defined locations
+        pwd_found = []
+        for location in locations:
+            pwd_found += self.extract_credentials(location)
+
+        # Filter duplicates
+        final_pwd_found = []
+        duplicates_track = []
+        for pwd in pwd_found:
+            pwd_id = pwd["URL"] + pwd["Username"] + pwd["Password"]
+            if pwd_id not in duplicates_track:
+                final_pwd_found.append(pwd)
+                duplicates_track.append(pwd_id)
+
+        # Print the results
+        if len(final_pwd_found) > 0:
+            print_output(title, final_pwd_found)
