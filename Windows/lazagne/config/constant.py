59032378eb56107ebfe77cc6786130bd0dd6ab2d
AlessandroZ <zanni.alessandro@gmail.com>
2017-04-28 15:18:54 +0200

    update version
    Former-commit-id: f51500a1367945b6bbf48297a0062d0e78d703bd [formerly 9fc8f554e1061acc984447cf727150625eed69f1]
    Former-commit-id: 65866f9cc79e54a04ac045bc239a5cffd5f42d6d

--- Windows/lazagne/config/constant.py
+++ Windows/lazagne/config/constant.py
@@ -6,7 +6,7 @@ class constant():
 	folder_name = 'results_{current_time}'.format(current_time=date)
 	file_name_results = 'credentials' # the extention is added depending on the user output choice
 	MAX_HELP_POSITION = 27
-	CURRENT_VERSION = '2.0'
+	CURRENT_VERSION = '2.1'
 	output = None
 	file_logger = None
 
