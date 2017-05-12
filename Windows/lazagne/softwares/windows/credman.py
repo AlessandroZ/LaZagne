704ae576181e96ecdd2c0360d5e768fffd4f7177
AlessandroZ <zanni.alessandro@gmail.com>
2017-05-13 01:46:04 +0200

    credman error fixed
    Former-commit-id: 3a33ebad5a96b6aa1fb5dcb18de2896b6aebe249 [formerly 3a33ebad5a96b6aa1fb5dcb18de2896b6aebe249 [formerly 3a33ebad5a96b6aa1fb5dcb18de2896b6aebe249 [formerly 93df4bb05e958735210eea5f7096642a975cc001]]]
    Former-commit-id: 28fd915de079b7a8600cf933111d41c1d7233ce4
    Former-commit-id: 57367733ee09b95d3488e9fafa3a0467d27ac8e0
    Former-commit-id: 748e7d5dd28afb7879830c9c52a8af8da9f05e2e

--- Windows/lazagne/softwares/windows/credman.py
+++ Windows/lazagne/softwares/windows/credman.py
@@ -11,30 +11,18 @@ class Credman(ModuleInfo):
 	# 	entropy = 'abe2869f-9b47-4cd9-a358-c22904dba7f7\0' # FOR CRED_TYPE_GENERIC
 	# 	entropy = '82BD0E67-9FEA-4748-8672-D5EFE5B779B0\0' # FOR CRED_TYPE_DOMAIN_VISIBLE_PASSWORD
 	
-	def run(self, software_name = None):		
+	def run(self, software_name = None):
 		pwdFound = []
 		creds = POINTER(PCREDENTIAL)()
 		count = c_ulong()
-		print_debug('DEBUG', 'Run Credential manager function')
 		if CredEnumerate(None, 0, byref(count), byref(creds)) == 1:
-			print_debug('DEBUG', 'CredEnumerate executed successfully')
-			print_debug('DEBUG', 'cound variable: %s' % str(count))
-			print_debug('DEBUG', 'Number of value: %s' % str(count.value))
 			for i in range(count.value):
-				print_debug('DEBUG', 'cred[i] variable: %s' % str(creds[i]))
 				c = creds[i].contents
-				print_debug('DEBUG', 'c variable: %s' % str(c))
 				if c.Type == CRED_TYPE_GENERIC or c.Type == CRED_TYPE_DOMAIN_VISIBLE_PASSWORD:
-					print_debug('DEBUG', 'value to decrypt found')
 					# For XP:
 					# - password are encrypted with specific salt depending on its Type
 					# - call CryptUnprotectData(byref(blobIn), None, byref(blobEntropy), None, None, CRYPTPROTECT_UI_FORBIDDEN, byref(blobOut))
 
-					print_debug('DEBUG', 'target: %s' % str(c.TargetName))
-					print_debug('DEBUG', 'username: %s' % str(c.UserName))
-					print_debug('DEBUG', 'password: %s' % str(c.CredentialBlob))
-					print_debug('DEBUG', 'size of the password: %s' % str(c.CredentialBlobSize.real))
-					print_debug('DEBUG', 'everything ok')
 					pwdFound.append(
 						{
 							'URL'		:	c.TargetName, 
@@ -42,9 +30,5 @@ class Credman(ModuleInfo):
 							'Password'	:	c.CredentialBlob[:c.CredentialBlobSize.real].replace('\x00', '')
 						}
 					)
-			print_debug('DEBUG', 'trying to free the handle')
 			CredFree(creds)
 		return pwdFound
-
-
-
