9f78d702291b7c2a3f5bcb8cd09f34fd3633c768
Oleksii Shevchuk <alxchk@gmail.com>
2017-05-23 22:55:07 +0300

    Add support for plain-text Chrome passwords on Linux
    Former-commit-id: 82734517586787ebc524f90e8d265da689c76390 [formerly 858228c2f5aaf527c7bd33ee4b6ba5b8733ec4d9]
    Former-commit-id: 18af4ce18f824eda9e9663d3210a686e516a5628

---
+++ Linux/lazagne/softwares/browsers/chrome.py
@@ -0,0 +1,56 @@
+# For non-keyring storage
+
+from lazagne.config.constant import *
+from lazagne.config.write_output import print_debug
+from lazagne.config.moduleInfo import ModuleInfo
+from lazagne.config import homes
+
+import sqlite3
+import tempfile
+
+class Chrome(ModuleInfo):
+    def __init__(self):
+		options = {'command': '-C', 'action': 'store_true', 'dest': 'chrome', 'help': 'chrome'}
+		ModuleInfo.__init__(self, 'chrome', 'browsers', options)
+
+    def get_paths(self):
+        return homes.get(file=[
+            '.config/google-chrome/Default/Login Data',
+            '.config/chromium/Default/Login Data'
+        ])
+
+    def get_logins(self, path):
+        try:
+            conn = sqlite3.connect(path)
+        except:
+            return
+
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute('SELECT origin_url,username_value,password_value FROM logins')
+            for url, user, password in cursor:
+                print url, user, password
+                yield {
+                    'URL': url,
+                    'Login': user,
+                    'Password': password
+                }
+        except:
+            pass
+
+        finally:
+            cursor.close()
+            conn.close()
+
+    def run(self, software_name = None):
+        all_passwords = []
+        for path in self.get_paths():
+            with tempfile.NamedTemporaryFile() as tmp:
+                with open(path) as infile:
+                    tmp.write(infile.read())
+
+                for pw in self.get_logins(tmp.name):
+                    all_passwords.append(pw)
+
+        return all_passwords
