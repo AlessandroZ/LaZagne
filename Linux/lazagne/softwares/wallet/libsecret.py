fefa8da37a257d91848778fbc37fd97f5f6a234d
AlessandroZ <zanni.alessandro@gmail.com>
2017-05-25 01:14:27 +0200

    retrieve libsecrets for all users when sudo

--- Linux/lazagne/softwares/wallet/libsecret.py
+++ Linux/lazagne/softwares/wallet/libsecret.py
@@ -6,42 +6,59 @@ from lazagne.config import homes
 
 class libsecret(ModuleInfo):
     def __init__(self):
-        options = {'command': '-l', 'action': 'store_true', 'dest': 'libsecret', 'help': 'libsecret'}
+        options = {'command': '-l', 'action': 'store_true', 'dest': 'SecretStorage', 'help': 'SecretStorage'}
         ModuleInfo.__init__(self, 'libsecret', 'wallet', options)
 
     def run(self, software_name = None):
         items = []
+        visited = set()
         try:
-            import secretstorage
             import dbus
+            import secretstorage
             import datetime
         except Exception as e:
             print_debug('ERROR', 'libsecret: {0}'.format(e))
             return []
 
-        for session in homes.sessions():
+        for _, session in homes.sessions():
             try:
-                dbus_session = dbus.SessionBus()
+                bus = dbus.bus.BusConnection(session)
+                collections = list(secretstorage.collection.get_all_collections(bus))
             except Exception, e:
                 print e
                 continue
 
-            try:
-                storage = secretstorage.Collection(dbus_session).get_all_items()
-            except Exception, e:
-                print e
-                continue
+            for collection in collections:
+                if collection.is_locked():
+                    continue
 
-            for item in storage:
-                values = {
-                    'created': str(datetime.datetime.fromtimestamp(item.get_created())),
-                    'modified': str(datetime.datetime.fromtimestamp(item.get_modified())),
-                    'content-type': item.get_secret_content_type(),
-                    'label': item.get_label(),
-                    'Password': item.get_secret(),
-                }
-                for k, v in item.get_attributes().iteritems():
-                    values[str(k)] = str(v)
-                items.append(values)
+                label = collection.get_label()
+                if label in visited:
+                    continue
+
+                visited.add(label)
+
+                try:
+                    storage = collection.get_all_items()
+                except Exception, e:
+                    print e
+                    continue
+
+                for item in storage:
+                    values = {
+                        'created': str(datetime.datetime.fromtimestamp(item.get_created())),
+                        'modified': str(datetime.datetime.fromtimestamp(item.get_modified())),
+                        'content-type': item.get_secret_content_type(),
+                        'label': item.get_label(),
+                        'Password': item.get_secret(),
+                        'collection': label,
+                    }
+
+                    for k, v in item.get_attributes().iteritems():
+                        values[unicode(k)] = unicode(v)
+                    items.append(values)
+
+            bus.flush()
+            bus.close()
 
         return items
