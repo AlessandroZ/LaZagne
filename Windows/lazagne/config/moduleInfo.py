8ee6b536ddaed3b4b4bc28460711c4023a3909fb
AlessandroZ <zanni.alessandro@gmail.com>
2017-08-31 13:53:04 +0200

    remove unecessary code
    Former-commit-id: 2e1c5c98249c0a3f93b2a010b4c388338c57e847 [formerly 2e1c5c98249c0a3f93b2a010b4c388338c57e847 [formerly 2e1c5c98249c0a3f93b2a010b4c388338c57e847 [formerly 145169b1521d57e43f8f0a2361630ea43b4e9afb]]]
    Former-commit-id: 25fb146a8059519ec2fd6c198ede18195b34aadb
    Former-commit-id: 949e0982f079c09d3e9d2b55f2aeff53cae73729
    Former-commit-id: 3623b624216c89d4b89c5126dd54fdfd425dd232

--- Windows/lazagne/config/moduleInfo.py
+++ Windows/lazagne/config/moduleInfo.py
@@ -12,7 +12,7 @@
 #		options['help'] = 'skype'
 
 class ModuleInfo():
-	def __init__(self, name, category, options, suboptions = [], need_high_privileges=False, need_system_privileges=False, need_to_be_in_env=True, cannot_be_impersonate_using_tokens=False):
+	def __init__(self, name, category, options, suboptions=[], need_high_privileges=False, need_system_privileges=False, need_to_be_in_env=True, cannot_be_impersonate_using_tokens=False):
 		self.name = name
 		self.category = category
 		self.options = options
@@ -21,27 +21,3 @@ class ModuleInfo():
 		self.need_system_privileges = need_system_privileges
 		self.need_to_be_in_env = need_to_be_in_env
 		self.cannot_be_impersonate_using_tokens = cannot_be_impersonate_using_tokens
-	
-	def name(self):
-		return self.name
-	
-	def category(self):
-		return self.category
-	
-	def options(self):
-		return self.options
-	
-	def suboptions(self):
-		return self.suboptions
-	
-	def need_high_privileges(self):
-		return self.need_high_privileges
-
-	def need_system_privileges(self):
-		return self.need_system_privileges
-	
-	def need_to_be_in_env(self):
-		return self.need_to_be_in_env
-
-	def cannot_be_impersonate_using_tokens(self):
-		return self.cannot_be_impersonate_using_tokens
