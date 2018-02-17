dbd806eeefb9fafb3da166eafe0206bc3f6224f5
Jack James <36520459+m41nt41n3r@users.noreply.github.com>
2018-02-17 22:36:41 +0300

    Add files via upload
    Former-commit-id: a302b88cc20167f05d7e16aa98bb901455f44a11 [formerly 1d28eb835605e061fbb7320c3b845fbf38c003c9]
    Former-commit-id: ffd20b839de5517eb9c2db962a9e271b2e753887

---
+++ Windows/lazagne/softwares/databases/postgresql.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- 
+from lazagne.config.write_output import print_debug
+from lazagne.config.moduleInfo import ModuleInfo
+from lazagne.config.constant import *
+import os
+
+class PostgreSQL(ModuleInfo):
+	def __init__(self):
+		ModuleInfo.__init__(self, name='postgresql', category='databases')
+
+	def run(self, software_name=None):
+		path = os.path.join(constant.profile['APPDATA'], u'postgresql', u'pgpass.conf')
+		if os.path.exists(path):
+			with open(path) as f:
+				data = f.readlines()
+
+				pwdFound = []
+				for line in data:
+					try:
+						items = line.strip().split(':')
+
+						values = {}
+						values['Hostname'] = items[0]
+						values['Port'] = items[1]
+						values['DB'] = items[2]
+						values['Username'] = items[3]
+						values['Password'] = items[4]
+
+						pwdFound.append(values)
+
+					except:
+						pass
+				
+				return pwdFound
