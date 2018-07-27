e77e2e44eed1c98ad99dca790be9c81831489b4f
AlessandroZ <zanni.alessandro@gmail.com>
2018-07-27 14:39:27 +0200

    Removing pycrypto dependency

--- Mac/lazagne/config/write_output.py
+++ Mac/lazagne/config/write_output.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*- 
-#!/usr/bin/python
-
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 from lazagne.config.constant import constant
 from time import gmtime, strftime
 import logging
@@ -10,9 +9,14 @@ import json
 import sys
 import os
 
-tmp_user = None
-
-# --------------------------- Standard output functions ---------------------------
+class bcolors():
+	HEADER 	= '\033[95m'
+	OKBLUE 	= '\033[94m'
+	OK 		= '\033[92m'
+	WARNING = '\033[96m'
+	FAIL 	= '\033[91m'
+	TITLE 	= '\033[93m'
+	ENDC 	= '\033[0m'
 
 class StandartOutput():
 	def __init__(self):
@@ -26,28 +30,46 @@ class StandartOutput():
 |====================================================================|
 		'''
 
-		self.grey_color 	= '\033[90m'
-		self.red_color 		= '\033[91m'
-		self.green_color 	= '\033[92m'
-		self.cyan_color		= '\033[94m'
-		self.no_color		= '\033[0m'
-
-	# print banner
+	def setColor(self, color=None):
+		b = bcolors()
+		if color == 'white':
+			sys.stdout.write(b.TITLE) 
+		elif color == 'red':
+			sys.stdout.write(b.FAIL)
+		elif color == 'green':
+			sys.stdout.write(b.OK)
+		elif color == 'cyan':
+			sys.stdout.write(b.WARNING)
+		else:
+			sys.stdout.write(b.ENDC)
+		
+	# Print banner
 	def first_title(self):
 		self.do_print(message=self.banner, color='white')
-	
-	# info option for the logging
-	def print_title(self, title):
-		t = '------------------- ' + title + ' passwords -----------------\n'
-		self.do_print(message=t, color='white')
-	
-	# debug option for the logging
-	def title_info(self, title):
-		t = '------------------- ' + title + ' passwords -----------------\n'
-		self.print_logging(function=logging.info, prefix='', message=t, color='white')
 
-	def print_user(self, user):
-		self.do_print('########## User: %s ##########\n' % user)
+	# Info option for the logging
+	def print_title(self, title):
+		t = u'------------------- ' + title + ' passwords -----------------\n'
+		self.do_print(message=t, color='white')
+
+	# Debug option for the logging
+	def title_info(self, title):
+		t = u'------------------- ' + title + ' passwords -----------------\n'
+		self.print_logging(function=logging.info, prefix='', message=t, color=False)
+
+	def write_header(self):
+		time 	= strftime("%Y-%m-%d %H:%M:%S", gmtime())
+		header 	= u'{banner}\r\n- Date: {date}\r\n- Username: {username}\r\n- Hostname:{hostname}\r\n\r\n'.format(
+					banner 	= self.banner.replace('\n', '\r\n'),
+					date 	= str(time), 
+					username= getpass.getuser(), 
+					hostname= socket.gethostname()
+				)
+		open(os.path.join(constant.folder_name, '{filename}.txt'.format(filename=constant.file_name_results)),"a+b").write(header)
+
+	def write_footer(self):
+		footer = '\n[+] %s passwords have been found.\r\n\r\n' % str(constant.nbPasswordFound)
+		open(os.path.join(constant.folder_name, '{filename}.txt'.format(filename=constant.file_name_results)),"a+b").write(footer)
 
 	def print_footer(self, elapsed_time=None):
 		footer = '\n[+] %s passwords have been found.\n' % str(constant.nbPasswordFound)
@@ -56,6 +78,22 @@ class StandartOutput():
 		if elapsed_time:
 			footer += '\nelapsed time = ' + str(elapsed_time)
 		self.do_print(footer)
+
+	def print_logging(self, function, prefix='[!]', message='', color=False):
+		if constant.quiet_mode:
+			return
+
+		try:
+			msg = u'{prefix} {msg}'.format(prefix=prefix, msg=message)
+		except:
+			msg = '{prefix} {msg}'.format(prefix=prefix, msg=str(message))
+
+		if color:
+			self.setColor(color)
+			function(msg)
+			self.setColor()
+		else:
+			function(msg)
 
 	def try_unicode(self, obj, encoding='utf-8'):
 		try:
@@ -66,151 +104,107 @@ class StandartOutput():
 			pass
 		return obj
 
-	# centralize print function
+	def print_without_error(self, message):
+		try:
+			print(message)
+		except:
+			print(repr(message))
+
+	# Centralize print function
 	def do_print(self, message='', color=False):
-		# quiet mode => nothing is printed
+		# Quiet mode => nothing is printed
 		if constant.quiet_mode:
 			return
 		
 		message = self.try_unicode(message)
-		self.print_without_error(message, color=color)
-
-	def choose_color(self, color):
-		if color == 'white':
-			return self.grey_color
-		elif color == 'red':
-			return self.red_color
-		elif color == 'green':
-			return self.green_color
-		elif color == 'cyan':
-			return self.cyan_color
+		if color:
+			self.setColor(color=color)
+			self.print_without_error(message)
+			self.setColor()
 		else:
-			return self.no_color
+			self.print_without_error(message)
 
-	def print_without_error(self, message, color):
-		print '{color}{message}{restore_color}'.format(
-			color=self.choose_color(color), 
-			message=message, 
-			restore_color=self.no_color
-		)
-
-	def print_logging(self, function, prefix='[!]', message='', color=False):
-		if constant.quiet_mode:
-			return
-
-		function('{color}{prefix} {message}{restore_color}'.format(
-			color=self.choose_color(color), 
-			prefix=prefix, 
-			message=message, 
-			restore_color=self.no_color)
-		)
-
-	def print_output(self, software_name, pwdFound, title1=False):
-	
-		# manage differently hashes / and hex value
-		if pwdFound:
-			category = None
-			if '__SYSTEM__' in pwdFound:
-				pwdFound.remove('__SYSTEM__')
-				category = 'hash'
-				pwdFound = pwdFound[0]
-
-		if pwdFound:
-			# if the debug logging level is not apply => print the title
-			if logging.getLogger().isEnabledFor(logging.INFO) == False:
-				# print the username only if password have been found
-				user = constant.finalResults.get('User', '')
-				global tmp_user
-				if user != tmp_user:
-					tmp_user = user
-					self.print_user(user)
-
-				# if not title1:
-				self.print_title(software_name)
-			
-			toWrite = []
-						
-			# System Hashes
-			if category == 'hash':
-				for pwd in pwdFound:
-					self.do_print(pwd)
-					toWrite.append(pwd)
-				self.do_print()
-
-			# Other passwords
-			else:
-				# Remove duplicated password
-				pwdFound = [dict(t) for t in set([tuple(d.items()) for d in pwdFound])]
-
-				for pwd in pwdFound:
-					password_category = False
-					# Detect which kinds of password has been found
-					lower_list = [s.lower() for s in pwd.keys()]
-					password = [s for s in lower_list if "password" in s]
-					if password: 
-						password_category = password
-					else:
-						key = [s for s in lower_list if "key" in s] # for the wifi
-						if key: 
-							password_category = key
-						else:
-							hash = [s for s in lower_list if "hash" in s]
-							if hash:
-								password_category = hash
-					
-					# No password found
-					if not password_category:
-						print_debug("FAILED", "Password not found !!!")
-					else:
-						print_debug("OK", '%s found !!!' % password_category[0].title())
-						toWrite.append(pwd)
-						
-						# Store all passwords found on a table => for dictionary attack if master password set
-						constant.nbPasswordFound += 1
-
-						if password_category[0] in pwd:
-							constant.passwordFound.append(pwd[password_category[0]])
-
-						elif password_category[0].capitalize() in pwd:
-							constant.passwordFound.append(pwd[password_category[0].capitalize()])
-					
-					for p in pwd.keys():
-						self.do_print('%s: %s' % (p, pwd[p]))
-					self.do_print()
-				
-			# write credentials into a text file
-			self.checks_write(toWrite, software_name)
-		else:
-			logging.info("[!] No passwords found\n")
-
-
-	def write_header(self):
-		time 	= strftime("%Y-%m-%d %H:%M:%S", gmtime())
-		header 	= '{banner}\r\n- Date: {date}\r\n- Username: {username}\r\n- Hostname:{hostname}\r\n\r\n'.format(
-			banner	 = self.banner.replace('\n', '\r\n'),
-			date 	 = str(time), 
-			username = getpass.getuser(), 
-			hostname = socket.gethostname()
-		)
-		open(os.path.join(constant.folder_name, '%s.txt' % constant.file_name_results),"a+b").write(header)
-
-	def write_footer(self):
-		footer = '\n[+] %s passwords have been found.\r\n\r\n' % str(constant.nbPasswordFound)
-		open(os.path.join(constant.folder_name, '%s.txt' % constant.file_name_results),"a+b").write(footer)
-	
 	def checks_write(self, values, category):
 		if values:
 			if "Passwords" not in constant.finalResults:
 				constant.finalResults["Passwords"] = []
 			constant.finalResults["Passwords"].append([{"Category": category}, values])
 
-def print_debug(error_level, message):
+	def print_output(self, software_name, pwdFound):
+		# Quiet mode => nothing is printed
+		if constant.quiet_mode:
+			return
 
-	# print when password is found
+		if pwdFound:
+			# If the debug logging level is not apply => print the title
+			if logging.getLogger().isEnabledFor(logging.INFO) == False:
+				self.print_title(software_name)
+
+			toWrite = []
+			
+			# Remove duplicated password
+			pwdFound = [dict(t) for t in set([tuple(d.items()) for d in pwdFound])]
+
+			for pwd in pwdFound:
+				password_category = False
+				# Detect which kinds of password has been found
+				lower_list 	= [s.lower() for s in pwd.keys()]
+				password 	= [s for s in lower_list if "password" in s]
+				
+				if password:
+					password_category = password
+				else:
+					key = [s for s in lower_list if "key" in s] # for the wifi
+					if key:
+						password_category = key
+					else:
+						hash = [s for s in lower_list if "hash" in s]
+						if hash:
+							password_category = hash
+						else:
+							cmd = [s for s in lower_list if "cmd" in s]
+							if cmd:
+								password_category = cmd
+
+				# Do not print empty passwords
+				try: 
+					if not pwd[password_category[0].capitalize()]:
+						continue
+				except:
+					pass
+
+				# No password found
+				if not password_category:
+					print_debug("FAILED", "Password not found !!!")
+				else:
+					print_debug("OK", '%s found !!!' % password_category[0].title())
+					toWrite.append(pwd)
+
+					# Store all passwords found on a table => for dictionary attack if master password set
+					constant.nbPasswordFound += 1
+					try:
+						passwd = pwd[password_category[0].capitalize()]
+						if passwd not in constant.passwordFound:
+							constant.passwordFound.append(passwd)
+					except:
+						pass
+
+				for p in pwd.keys():
+					self.do_print('%s: %s' % (p, pwd[p]))
+				self.do_print()
+
+			# Write credentials into a text file
+			self.checks_write(toWrite, software_name)
+		else:
+			logging.info("[!] No passwords found\n")
+
+
+def print_debug(error_level, message): 
+	# Print when password is found
 	if error_level == 'OK':
 		constant.st.do_print(message='[+] {message}'.format(message=message), color='green')
 
-	# print when password is not found
+	# Print when password is not found
 	elif error_level == 'FAILED':
 		constant.st.do_print(message='[-] {message}'.format(message=message), color='red')
 
@@ -221,42 +215,42 @@ def print_debug(error_level, message):
 		constant.st.print_logging(function=logging.warning, prefix='[!]', message=message, color='cyan')
 
 	elif error_level == 'DEBUG':
-		constant.st.print_logging(function=logging.debug, prefix='[!]', message=message)
+		constant.st.print_logging(function=logging.debug, message=message, prefix='[!]')
 
 	else:
-		constant.st.print_logging(function=logging.info, prefix='[!]', message=message)
+		constant.st.print_logging(function=logging.info, message=message, prefix='[!]')
 
 # --------------------------- End of output functions ---------------------------
 
 def parseJsonResultToBuffer(jsonString, color=False):
-	buffer = u''
+	green = ''
+	reset = ''
+	title = ''
+	if color:
+		b = bcolors()
+		green = b.OK
+		title = b.TITLE
+		reset = b.ENDC
+
+	buffer = ''
 	try:
 		for json in jsonString:
 			if json:
-				buffer += u'##################  User: {username} ################## \r\n'.format(username=json['User'])
 				if 'Passwords' not in json:
-					buffer += u'No passwords found for this user !\r\n\r\n'
+					buffer += 'No passwords found for this user !'
 				else:
 					for all_passwords in json['Passwords']:
-						buffer += u'\r\n------------------- {password_category} -----------------\r\n'.format(password_category=all_passwords[0]['Category'])
-						if all_passwords[0]['Category'].lower() in ['lsa', 'hashdump', 'cachedump']:
-							for dic in all_passwords[1]:
-								if all_passwords[0]['Category'].lower() == 'lsa':
-									for d in dic:
-										buffer += u'%s\r\n' % (constant.st.try_unicode(d))
-								else:
-									buffer += u'%s\r\n' % (constant.st.try_unicode(dic))
-						else:
-							for password_by_category in all_passwords[1]:
-								buffer += u'\r\nPassword found !!!\r\n'
-								for dic in password_by_category.keys():
-									try:
-										buffer += u'%s: %s\r\n' % (dic, constant.st.try_unicode(password_by_category[dic]))
-									except Exception, e:
-										print_debug('ERROR', 'Error retrieving the password encoding: %s' % e)
-						buffer += u'\r\n'
+						buffer += '{title_color}------------------- {password_category} -----------------{reset_color}\r\n'.format(title_color=title, password_category=all_passwords[0]['Category'], reset_color=reset)
+						for password_by_category in all_passwords[1]:
+							buffer += '\r\n{green_color}Password found !!!{reset_color}\r\n'.format(green_color=green, reset_color=reset)
+							for dic in password_by_category.keys():
+								try:
+									buffer += '%s: %s\r\n' % (dic, password_by_category[dic].encode('utf-8'))
+								except:
+									buffer += '%s: %s\r\n' % (dic, password_by_category[dic].encode(encoding='utf-8',errors='replace'))
+						buffer += '\r\n'
+
 	except Exception as e:
-		print_debug('ERROR', 'Error parsing the json results: %s' % e)
-		print_debug('ERROR', 'json content: %s' % jsonString)
+		print_debug('ERROR', u'Error parsing the json results: {error}'.format(error=e))
 
 	return buffer
