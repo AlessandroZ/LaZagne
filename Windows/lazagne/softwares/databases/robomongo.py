141ac5f83f1245d027c289433d8c7278f0bae9aa
unknown <anonymous@mail.com>
2018-08-02 00:32:56 +0300

    PEP8 some code
    Former-commit-id: 95ba3e1141e46080140ad3823fce06d7240f0cca [formerly 0c6283debccd826e9edfa955744e8b812f1a92fc]
    Former-commit-id: 987168539a9bf0737420dde8b318676f93884e20

--- Windows/lazagne/softwares/databases/robomongo.py
+++ Windows/lazagne/softwares/databases/robomongo.py
@@ -1,88 +1,102 @@
 # -*- coding: utf-8 -*- 
-from lazagne.config.write_output import print_debug
-from lazagne.config.module_info import ModuleInfo
-from lazagne.config.constant import *
 import json
 import os
 
+from lazagne.config.constant import constant
+from lazagne.config.module_info import ModuleInfo
+from lazagne.config.write_output import print_debug
+
+
 class Robomongo(ModuleInfo):
 
-	def __init__(self):
-		ModuleInfo.__init__(self, 'robomongo', 'databases')
-		
-		self.paths = [	
-						{	
-							'directory'	: u'.config/robomongo', 
-							'filename'	: u'robomongo.json',
-						},
-						{
-							'directory'	: u'.3T/robo-3t/1.1.1', 
-							'filename'	: u'robo3t.json',
-						}
-					]
+    def __init__(self):
+        ModuleInfo.__init__(self, 'robomongo', 'databases')
 
-	def read_file_content(self, file_path):
-		"""
-		Read the content of a file
+        self.paths = [
+            {
+                'directory': u'.config/robomongo',
+                'filename': u'robomongo.json',
+            },
+            {
+                'directory': u'.3T/robo-3t/1.1.1',
+                'filename': u'robo3t.json',
+            }
+        ]
 
-		:param file_path: Path of the file to read.
+    def read_file_content(self, file_path):
+        """
+        Read the content of a file
 
-		:return: File content as string.
-		"""
-		content = ""
-		if os.path.isfile(file_path):
-			with open(file_path, 'r') as file_handle:
-				content = file_handle.read()
+        :param file_path: Path of the file to read.
 
-		return content
+        :return: File content as string.
+        """
+        content = ""
+        if os.path.isfile(file_path):
+            with open(file_path, 'r') as file_handle:
+                content = file_handle.read()
 
-	def parse_json(self, connection_file_path):
-		repos_creds = []
-		try:
-			with open(connection_file_path) as connection_file:
-				connections_infos = json.load(connection_file)
-				for connection_infos in connections_infos["connections"]:
-					creds = {}
-					creds["Name"] = connection_infos["connectionName"]
-					creds["Host"] = connection_infos["serverHost"]
-					creds["Port"] = connection_infos["serverPort"]
-					if bool(connection_infos["credentials"][0]["enabled"]):
-						creds["AuthMode"] = "CREDENTIALS"
-						creds["DatabaseName"] = connection_infos["credentials"][0]["databaseName"]
-						creds["AuthMechanism"] = connection_infos["credentials"][0]["mechanism"]
-						creds["Login"] = connection_infos["credentials"][0]["userName"]
-						creds["Password"] = connection_infos["credentials"][0]["userPassword"]
-					else:
-						creds["Host"] = connection_infos["ssh"]["host"]
-						creds["Port"] = connection_infos["ssh"]["port"]
-						creds["Login"] = connection_infos["ssh"]["userName"]
-						if (bool(connection_infos["ssh"]["enabled"]) and
-									connection_infos["ssh"]["method"] == "password"):
-							creds["AuthMode"] = "SSH_CREDENTIALS"
-							creds["Password"] = connection_infos["ssh"]["userPassword"]
-						else:
-							creds["AuthMode"] = "SSH_PRIVATE_KEY"
-							creds["Passphrase"] = connection_infos["ssh"]["passphrase"]
-							creds["PrivateKey"] = self.read_file_content(connection_infos["ssh"]["privateKeyFile"])
-							creds["PublicKey"] = self.read_file_content(connection_infos["ssh"]["publicKeyFile"])
-					repos_creds.append(creds)
-		except Exception as e:
-			print_debug("ERROR", u"Cannot retrieve connections credentials '{error}'".format(error=e))
+        return content
 
-		return repos_creds 
+    def parse_json(self, connection_file_path):
+        repos_creds = []
+        if not os.path.exists(connection_file_path):
+            return repos_creds
+        with open(connection_file_path) as connection_file:
+            try:
+                connections_infos = json.load(connection_file)
+            except Exception:
+                return repos_creds
+            for connection in connections_infos.get("connections", []):
+                try:
+                    creds = {
+                        "Name": connection["connectionName"],
+                        "Host": connection["serverHost"],
+                        "Port": connection["serverPort"]
+                    }
+                    crd = connection["credentials"][0]
+                    if crd.get("enabled"):
+                        creds.update({
+                            "AuthMode": "CREDENTIALS",
+                            "DatabaseName": crd["databaseName"],
+                            "AuthMechanism": crd["mechanism"],
+                            "Login": crd["userName"],
+                            "Password": crd["userPassword"]
+                        })
+                    else:
+                        creds.update({
+                            "Host": connection["ssh"]["host"],
+                            "Port": connection["ssh"]["port"],
+                            "Login": connection["ssh"]["userName"]
+                        })
+                        if connection["ssh"]["enabled"] and connection["ssh"]["method"] == "password":
+                            creds.update({
+                                "AuthMode": "SSH_CREDENTIALS",
+                                "Password": connection["ssh"]["userPassword"]
+                            })
+                        else:
+                            creds.update({
+                                "AuthMode": "SSH_PRIVATE_KEY",
+                                "Passphrase": connection["ssh"]["passphrase"],
+                                "PrivateKey": self.read_file_content(connection["ssh"]["privateKeyFile"]),
+                                "PublicKey": self.read_file_content(connection["ssh"]["publicKeyFile"])
+                            })
+                    repos_creds.append(creds)
+                except Exception as e:
+                    print_debug("ERROR", u"Cannot retrieve connections credentials '{error}'".format(error=e))
 
-	def run(self, software_name=None):
-		"""
-		Extract all connection's credentials.
+        return repos_creds
 
-		:return: List of dict in which one dict contains all information for a connection.
-		"""
+    def run(self, software_name=None):
+        """
+        Extract all connection's credentials.
 
-		for file in self.paths:
-			if os.path.exists(os.path.join(constant.profile['USERPROFILE'], file['filename'])):
-				return self.parse_json(os.path.join(path, file['filename']))
-
-		for directory in self.paths:
-			connection_file_path = os.path.join(constant.profile['USERPROFILE'], directory['directory'], directory['filename'])
-			if os.path.exists(connection_file_path):
-				return self.parse_json(connection_file_path)
+        :return: List of dict in which one dict contains all information for a connection.
+        """
+        pwd_found = []
+        for directory in self.paths:
+            connection_file_path = os.path.join(constant.profile['USERPROFILE'],
+                                                directory['directory'],
+                                                directory['filename'])
+            pwd_found.extend(self.parse_json(connection_file_path))
+        return pwd_found
--- Windows/lazagne/softwares/games/roguestale.py
+++ Windows/lazagne/softwares/games/roguestale.py
@@ -1,42 +1,42 @@
 # -*- coding: utf-8 -*- 
-from lazagne.config.write_output import print_debug
-from lazagne.config.module_info import ModuleInfo
-from lazagne.config.constant import *
-import xml.etree.cElementTree as ET
 import os
 import re
+from xml.etree.cElementTree import ElementTree
+
+from lazagne.config.constant import constant
+from lazagne.config.module_info import ModuleInfo
+from lazagne.config.write_output import print_debug
+
 
 class RoguesTale(ModuleInfo):
-	def __init__(self):
-		ModuleInfo.__init__(self, 'roguestale', 'games')
-		
-	def run(self, software_name=None):
-		creds 		= []
-		directory 	= constant.profile['USERPROFILE'] + u'\\Documents\\Rogue\'s Tale\\users'
-		
-		# The actual user details are stored in *.userdata files
-		if os.path.exists(directory):
-			files = os.listdir(directory)
-			
-			for file in files:
-				if re.match('.*\.userdata',file):
-					# We've found a user file, now extract the hash and username				
-					
-					xmlfile = directory + '\\' + file
-					tree = ET.ElementTree(file=xmlfile)
-					root = tree.getroot()
-					
-					# Double check to make sure that the file is valid
-					if root.tag != 'user':
-						print_debug(u'Profile %s does not appear to be valid' % file)
-						continue
-					
-					# Now save it to credentials
-					creds.append(
-						{
-							'Login'	: root.attrib['username'], 
-							'Hash'	: root.attrib['password']
-						}
-					)
-			
-			return creds
+    def __init__(self):
+        ModuleInfo.__init__(self, 'roguestale', 'games')
+
+    def run(self, software_name=None):
+        creds = []
+        directory = constant.profile['USERPROFILE'] + u'\\Documents\\Rogue\'s Tale\\users'
+
+        # The actual user details are stored in *.userdata files
+        if os.path.exists(directory):
+            files = os.listdir(directory)
+
+            for f in files:
+                if re.match('.*\.userdata', f):
+                    # We've found a user file, now extract the hash and username
+
+                    xmlfile = directory + '\\' + f
+                    tree = ElementTree(file=xmlfile)
+                    root = tree.getroot()
+
+                    # Double check to make sure that the file is valid
+                    if root.tag != 'user':
+                        print_debug('WARNING', u'Profile %s does not appear to be valid' % f)
+                        continue
+
+                    # Now save it to credentials
+                    creds.append({
+                        'Login': root.attrib['username'],
+                        'Hash': root.attrib['password']
+                    })
+
+            return creds
