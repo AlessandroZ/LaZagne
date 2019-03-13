68229b324c4576e880f025e9427010fb8e757b51
MoHaMmaD......♫♫ <khosravim861@gmail.com>
2019-03-13 15:11:45 +0330

    Remove duplicated directory

--- lazagne/config/lib/memorpy/structures.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF8 -*-
-
-import sys
-if sys.platform=="win32":
-    from .WinStructures import *
-else:
-    from .LinStructures import *
--- lazagne/config/lib/memorpy/version.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF8 -*-
-
-version=(1,7)
-version_string="%s.%s"%version
-
