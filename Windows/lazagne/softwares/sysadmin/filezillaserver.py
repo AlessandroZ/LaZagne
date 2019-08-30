5706c25da9e139cdf070e774864e063d8d377c3a
AlessandroZ <zanni.alessandro@gmail.com>
2019-08-30 10:35:09 +0200

    fix travis

--- Windows/lazagne/softwares/sysadmin/filezillaserver.py
+++ Windows/lazagne/softwares/sysadmin/filezillaserver.py
@@ -22,23 +22,21 @@ class FilezillaServer(ModuleInfo):
             if os.path.exists(xml_file):
                 tree = ElementTree(file=xml_file)
                 root = tree.getroot()
+                host = port = password = None
 
                 for item in root.iter("Item"):
                     if item.attrib['name'] == 'Last Server Address':
-				        host = item.text
+                        host = item.text
                     elif item.attrib['name'] == 'Last Server Port':
-				        port = item.text
+                        port = item.text
                     elif item.attrib['name'] == 'Last Server Password':
-				        password = item.text        
+                        password = item.text
                 # if all((host, port, login)) does not work
                 if host is not None and port is not None and password is not None:
-                    values = {
+                    pwd_found = [{
                         'Host': host,
                         'Port': port,
                         'Password': password,
-                    }
+                    }]
 
-            if values: 
-                pwd_found.append(values)
-
-        return pwd_found
+            return pwd_found
