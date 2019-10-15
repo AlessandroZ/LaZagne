f6e0f2dbfe62dc7de78fb81b03a05ad7f9207fa9
Cervoise <antoine.cervoise@gmail.com>
2019-10-15 20:56:05 +0200

    Update keepassconfig.py
    Corrected two bugs
    Handle ..\..\ as C drive
    Remove duplicate for KeePass 1
    Check type of DB for KeePass 2 when possible (NTLM, Password, Keyfile)

--- Windows/lazagne/softwares/sysadmin/keepassconfig.py
+++ Windows/lazagne/softwares/sysadmin/keepassconfig.py
@@ -10,7 +10,7 @@ class KeePassConfig(ModuleInfo):
 
     def __init__(self):
         ModuleInfo.__init__(self, 'keepassconfig', 'sysadmin')
-        self.attr_to_extract = ["Keyfile", "Database"]
+        self.attr_to_extract = ["Keyfile", "Database", "Type"]
 
     def run(self):
         """
@@ -25,23 +25,30 @@ class KeePassConfig(ModuleInfo):
             connection_file_location = os.path.join(connection_file_directory, u'KeePass.ini')
             if os.path.isfile(connection_file_location):
                 file_content = open(connection_file_location, 'r').read()
-                #KeeLastDb
-                if file_content.partition("KeeLastDb=")[1] == "KeeLastDb=":
-                    database = file_content.partition("KeeLastDb=")[2].partition('\n')[0]
-                    pwd_found.append({
-                        'Keyfile': "No keyfile found",
-                        'Database': database
-                    })
-                if file_content.split("KeeKeySourceID") > 1:
+                #KeeKeySourceID
+                if len(file_content.split("KeeKeySourceID")) > 1:
                     KeeKeySource_number = len(file_content.split("KeeKeySourceID")) - 1
                     for i in range(0, KeeKeySource_number ):
                         database = file_content.partition("KeeKeySourceID" + str(i) + "=" )[2].partition('\n')[0]
+                        database = database.replace('..\\..\\', 'C:\\')
                         keyfile = file_content.partition("KeeKeySourceValue" + str(i) + "=" )[2].partition('\n')[0]
                         pwd_found.append({
                             'Keyfile': keyfile,
                             'Database': database
                         })  
-
+                #KeeLastDb
+                if file_content.partition("KeeLastDb=")[1] == "KeeLastDb=":
+                    database = file_content.partition("KeeLastDb=")[2].partition('\n')[0]
+                    database = database.replace('..\\..\\', 'C:\\')
+                    already_in_pwd_found = 0
+                    for elmt in pwd_found:
+                        if database == elmt['Database']:
+                            already_in_pwd_found = 1
+                    if already_in_pwd_found == 0:
+                        pwd_found.append({
+                            'Keyfile': "No keyfile found",
+                            'Database': database
+                        })
         #Keepass2
         connection_file_directory = os.path.join(constant.profile['APPDATA'], u'KeePass')
         if os.path.exists(connection_file_directory):
@@ -52,11 +59,22 @@ class KeePassConfig(ModuleInfo):
                     connections = parse(connection_file_location).getroot()
                     connection_nodes = connections.findall(".//Association")
                     for connection_node in connection_nodes:
-                        database = connection_node.find('DatabasePath').text
-                        keyfile = connection_node.find('KeyFilePath').text
+                        database = connection_node.find('DatabasePath').text.replace('..\\..\\', 'C:\\')
+                        type = ""
+                        if connection_node.find('Password') is not None:
+                            type += "Password - "
+                        if  connection_node.find('UserAccount') is not None:
+                            type += "NTLM - "
+                        try:
+                            keyfile = connection_node.find('KeyFilePath').text.replace('..\\..\\', 'C:\\')
+                            type += "Keyfile - "
+                        except:
+                            keyfile = "No keyfile found"
+
                         pwd_found.append({
                             'Keyfile': keyfile,
-                            'Database': database
+                            'Database': database,
+							'Type': type[:-3]
                         })
                 except:
                     pass
@@ -65,7 +83,7 @@ class KeePassConfig(ModuleInfo):
                     connections = parse(connection_file_location).getroot()
                     connection_nodes = connections.findall(".//LastUsedFile")
                     for connection_node in connection_nodes:
-                        database = connection_node.find('Path').text
+                        database = connection_node.find('Path').text.replace('..\\..\\', 'C:\\')
                         already_in_pwd_found = 0
                         for elmt in pwd_found:
                             if database == elmt['Database']:
@@ -82,7 +100,7 @@ class KeePassConfig(ModuleInfo):
                     connections = parse(connection_file_location).getroot()
                     connection_nodes = connections.findall(".//ConnectionInfo")
                     for connection_node in connection_nodes:
-                        database = connection_node.find('Path').text
+                        database = connection_node.find('Path').text.replace('..\\..\\', 'C:\\')
                         already_in_pwd_found = 0
                         for elmt in pwd_found:
                             if database == elmt['Database']:
