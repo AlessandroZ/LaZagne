fbdb42d02a6562be09c2b52acfc56d1642f039c1
Artem <artem-belocerkov@yandex.ru>
2021-09-16 01:50:42 -0700

    Do not use modules with broken depensences for MAC.

--- Mac/lazagne/config/manage_modules.py
+++ Mac/lazagne/config/manage_modules.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*- 
 # !/usr/bin/python
-
-# browsers
-from lazagne.softwares.browsers.mozilla import firefox_browsers
-from lazagne.softwares.browsers.chrome import Chrome
-
-# mails 
-from lazagne.softwares.mails.thunderbird import Thunderbird
-
-# system
-from lazagne.softwares.system.hashdump import HashDump
-from lazagne.softwares.system.chainbreaker import ChainBreaker
-from lazagne.softwares.system.system import System
+from lazagne.config.soft_import_module import soft_import
+from lazagne.softwares.browsers.firefox_browsers import firefox_browsers
 
 
 def get_categories():
@@ -19,16 +9,20 @@ def get_categories():
         'browsers': {'help': 'Web browsers supported'},
         'mails': {'help': 'Email clients supported'},
         'system': {'help': 'System credentials'},
+        'unused': {'help': 'This modules could not be used because of broken dependence'}
     }
     return category
 
 
 def get_modules():
     module_names = [
-        Thunderbird(),
-        Chrome(),
-        HashDump(),
-        ChainBreaker(),
-        System()
+        # system
+        soft_import("lazagne.softwares.system.hashdump", "HashDump")(),
+        soft_import("lazagne.softwares.system.chainbreaker", "ChainBreaker")(),
+        soft_import("lazagne.softwares.system.system", "System")(),
+        # mails
+        soft_import("lazagne.softwares.mails.thunderbird", "Thunderbird")(),
+        # browsers
+        soft_import("lazagne.softwares.browsers.chrome", "Chrome")(),
     ]
     return module_names + firefox_browsers
