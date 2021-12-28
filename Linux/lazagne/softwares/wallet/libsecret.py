64ba71479b0ae349ab6448ce03dc490d0e6dfc25
plonibarploni <44826203+plonibarploni@users.noreply.github.com>
2021-12-27 22:46:53 -0500

    jeepney.integrate.blocking is deprecated

--- Linux/lazagne/softwares/wallet/libsecret.py
+++ Linux/lazagne/softwares/wallet/libsecret.py
@@ -42,6 +42,50 @@ class Libsecret(ModuleInfo):
                     continue
             except Exception:
                 self.error(traceback.format_exc())
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from lazagne.config.constant import constant
+from lazagne.config.module_info import ModuleInfo
+from lazagne.config import homes
+from binascii import hexlify
+import traceback
+
+try:
+    import jeepney.auth
+# except ImportError:
+except Exception:
+    pass
+else:
+    # Thanks to @mitya57 for its Work around 
+    def make_auth_external():
+        hex_uid = hexlify(str(make_auth_external.uid).encode('ascii'))
+        return b'AUTH EXTERNAL %b\r\n' % hex_uid
+    jeepney.auth.make_auth_external = make_auth_external
+
+
+class Libsecret(ModuleInfo):
+    def __init__(self):
+        ModuleInfo.__init__(self, 'libsecret', 'wallet')
+
+    def run(self):
+        items = []
+        visited = set()
+        try:
+            import dbus
+            import secretstorage
+            import datetime
+        except ImportError as e:
+            self.error('libsecret: {0}'.format(e))
+            return []
+
+        for uid, session in homes.sessions():
+            try:
+                # List bus connection names
+                bus = dbus.bus.BusConnection(session)
+                if 'org.freedesktop.secrets' not in [str(x) for x in bus.list_names()]:
+                    continue
+            except Exception:
+                self.error(traceback.format_exc())
                 continue
 
             collections = None
@@ -54,9 +98,9 @@ class Libsecret(ModuleInfo):
             if not collections:
                 try:
                     # Python 3
-                    from jeepney.integrate.blocking import connect_and_authenticate
+                    from jeepney.io.blocking import open_dbus_connection
                     make_auth_external.uid = uid
-                    bus = connect_and_authenticate(session)
+                    bus = open_dbus_connection(session)
                     collections = secretstorage.get_all_collections(bus)
                 except Exception:
                     self.error(traceback.format_exc())
